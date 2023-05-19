b32fb0717b4c9e7425ac3a45226d696be81a064d
Oshan-Tharuka <ms22043900@my.sliit.lk>
2023-05-19 18:03:44 +0530

    Developed & Bind CultivationPlanAdapter to the RecyclerView of CultivationPlanFragment

---
+++ app/src/main/java/com/radproject/hms/listAdapters/CultivationPlanAdapter.java
@@ -0,0 +1,63 @@
+package com.radproject.hms.listAdapters;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.radproject.hms.R;
+import com.radproject.hms.models.CultivationPlanModel;
+
+import java.util.ArrayList;
+
+public class CultivationPlanAdapter extends RecyclerView.Adapter<CultivationPlanAdapter.ViewHolder> {
+
+    private ArrayList<CultivationPlanModel> cultivationPlanList;
+
+    public CultivationPlanAdapter(ArrayList<CultivationPlanModel> cultivationPlanList) {
+        this.cultivationPlanList = cultivationPlanList;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.list_item_cultivation_plan, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        CultivationPlanModel cultivationPlan = cultivationPlanList.get(position);
+
+        holder.cultivationPlanNameTextView.setText(cultivationPlan.getCultivation_Plan_name());
+        holder.cultivationCropIdTextView.setText(cultivationPlan.getCultivation_CROP_ID());
+        holder.cultivationStartDateTextView.setText(cultivationPlan.getCultivation_Start_date());
+        holder.cultivationEndDateTextView.setText(cultivationPlan.getCultivation_End_date());
+        holder.cultivationStatusTextView.setText(cultivationPlan.getStatus());
+    }
+
+    @Override
+    public int getItemCount() {
+        return cultivationPlanList.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        TextView cultivationPlanNameTextView;
+        TextView cultivationCropIdTextView;
+        TextView cultivationStartDateTextView;
+        TextView cultivationEndDateTextView;
+        TextView cultivationStatusTextView;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            cultivationPlanNameTextView = itemView.findViewById(R.id.cultivationPlanNameTextView);
+            cultivationCropIdTextView = itemView.findViewById(R.id.cultivationCropIdTextView);
+            cultivationStartDateTextView = itemView.findViewById(R.id.cultivationStartDateTextView);
+            cultivationEndDateTextView = itemView.findViewById(R.id.cultivationEndDateTextView);
+            cultivationStatusTextView = itemView.findViewById(R.id.cultivationStatusTextView);
+        }
+    }
+}
--- app/src/main/java/com/radproject/hms/screensFragments/CultivationPlanFragment.java
+++ app/src/main/java/com/radproject/hms/screensFragments/CultivationPlanFragment.java
@@ -8,24 +8,37 @@ import android.view.ViewGroup;
 import android.widget.Button;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.radproject.hms.R;
+import com.radproject.hms.listAdapters.CultivationPlanAdapter;
+import com.radproject.hms.models.CultivationPlanModel;
 import com.radproject.hms.subActivities.CultivationPlanActivity;
+
+import java.util.ArrayList;
 
 public class CultivationPlanFragment extends Fragment {
 
     private Button addNewButton;
+    private RecyclerView cultivationRecyclerView;
+    private CultivationPlanAdapter cultivationAdapter;
+    private ArrayList<CultivationPlanModel> cultivationPlanList;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_cultivation_plan, container, false);
         initViews(view);
         initClicks();
+        setupRecyclerView();
         return view;
     }
+
     private void initViews(View view) {
         addNewButton = view.findViewById(R.id.cultivationF_addNew_btn);
-
+        cultivationRecyclerView = view.findViewById(R.id.cultivationF_RV);
     }
+
     private void initClicks() {
         addNewButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -35,5 +48,34 @@ public class CultivationPlanFragment extends Fragment {
                 startActivity(intent);
             }
         });
+    }
+
+    private void setupRecyclerView() {
+        cultivationPlanList = new ArrayList<>(); // Initialize your list of cultivation plans
+
+        // Create dummy data (replace with your actual data)
+        CultivationPlanModel cultivationPlan1 = new CultivationPlanModel();
+        cultivationPlan1.setCultivation_Plan_name("Plan 1");
+        cultivationPlan1.setCultivation_CROP_ID("Crop 1");
+        cultivationPlan1.setCultivation_Start_date("2023-05-19");
+        cultivationPlan1.setCultivation_End_date("2023-06-19");
+        cultivationPlan1.setStatus("Active");
+        cultivationPlanList.add(cultivationPlan1);
+
+        CultivationPlanModel cultivationPlan2 = new CultivationPlanModel();
+        cultivationPlan2.setCultivation_Plan_name("Plan 2");
+        cultivationPlan2.setCultivation_CROP_ID("Crop 2");
+        cultivationPlan2.setCultivation_Start_date("2023-06-20");
+        cultivationPlan2.setCultivation_End_date("2023-07-20");
+        cultivationPlan2.setStatus("Inactive");
+        cultivationPlanList.add(cultivationPlan2);
+
+        // Create and set the adapter
+        cultivationAdapter = new CultivationPlanAdapter(cultivationPlanList);
+        cultivationRecyclerView.setAdapter(cultivationAdapter);
+
+        // Set the layout manager
+        LinearLayoutManager layoutManager = new LinearLayoutManager(getActivity());
+        cultivationRecyclerView.setLayoutManager(layoutManager);
     }
 }
