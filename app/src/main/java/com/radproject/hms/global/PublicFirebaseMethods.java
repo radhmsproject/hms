256b5d7f60f8da2fa00ed288efe0b427fec55067
Oshan-Tharuka <ms22043900@my.sliit.lk>
2023-05-19 18:15:53 +0530

    Get cultivation data from firebase.

--- app/src/main/java/com/radproject/hms/global/PublicFirebaseMethods.java
+++ app/src/main/java/com/radproject/hms/global/PublicFirebaseMethods.java
@@ -1,19 +1,30 @@
 package com.radproject.hms.global;
 
 import static com.radproject.hms.global.GlobalVariables.db;
+import static com.radproject.hms.global.GlobalVariables.uid;
 
 import android.content.Context;
+import android.util.Log;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
+import com.radproject.hms.models.CropModel;
+import com.radproject.hms.models.CultivationPlanModel;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class PublicFirebaseMethods {
 
@@ -70,5 +81,26 @@ public class PublicFirebaseMethods {
             });
             return cultivationPlanId;
         }
+
+
+    public static ArrayList<CultivationPlanModel> getAllCultivationForCurrentFarmer() {
+        CollectionReference cultivationPlanRef = db.collection("Farmer").document(uid).collection("cultivation_plan");
+        ArrayList<CultivationPlanModel> cultivationList = new ArrayList<>();
+        cultivationPlanRef.get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                if (task.isSuccessful()) {
+                    for (QueryDocumentSnapshot document : task.getResult()) {
+                        CultivationPlanModel cropModel = document.toObject(CultivationPlanModel.class);
+                        cultivationList.add(cropModel);
+
+                    }
+                } else {
+                    Log.e("Firestore", "Error getting crops: " + task.getException());
+                }
+            }
+        });
+        return cultivationList;
+    }
 
 }
