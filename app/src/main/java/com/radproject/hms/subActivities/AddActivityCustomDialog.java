032aed3335afdaa1efb2148555cce6388e0f9ac9
nomadcorpz <nomadcooperation@gmail.com>
2023-05-27 15:26:59 +0530

    Add Activity Custom - Updated

--- app/src/main/java/com/radproject/hms/subActivities/AddActivityCustomDialog.java
+++ app/src/main/java/com/radproject/hms/subActivities/AddActivityCustomDialog.java
@@ -1,10 +1,14 @@
 package com.radproject.hms.subActivities;
 
+import static android.content.ContentValues.TAG;
+
 import android.app.Dialog;
 import android.content.Context;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.view.Window;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ImageButton;
@@ -13,6 +17,7 @@ import android.widget.TextView;
 
 import com.radproject.hms.R;
 import com.radproject.hms.global.GlobalVariables;
+import com.radproject.hms.models.ActivityModel;
 import com.radproject.hms.models.CultivationPlanModel;
 import com.radproject.hms.models.FarmModel;
 
@@ -32,6 +37,9 @@ public class AddActivityCustomDialog extends Dialog {
     private TextView startDateTextView;
     private ImageButton startDateSelectImageButton;
     private Button addFarmButton;
+    CultivationPlanModel cultivationPlan = null;
+    String farmID;
+    String farmName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,9 +53,9 @@ public class AddActivityCustomDialog extends Dialog {
         startDateSelectImageButton = findViewById(R.id.start_select_calender_IB);
         addFarmButton = findViewById(R.id.btn_add_farm);
 
+        initClicks();
         initSActivitySpinner();
         // Retrieve the CultivationPlanModel from the dataBundle
-        CultivationPlanModel cultivationPlan = null;
         if (dataBundle != null) {
             cultivationPlan = (CultivationPlanModel) dataBundle.getSerializable("cultivationPlan");
             ArrayList<String> farm_id_list = cultivationPlan.getCultivation_Farm_ID_list();
@@ -55,15 +63,49 @@ public class AddActivityCustomDialog extends Dialog {
             for (String farmId : farm_id_list) {
                 for (FarmModel farm : GlobalVariables.all_farm_list) {
                     if (farm.getFarmId().equals(farmId)) {
-                        String farmIdAndName = farm.getFarmId() + " - " + farm.getName();
+                        String farmIdAndName = farm.getFarmId() + " : " + farm.getName();
                         filteredFarmNames.add(farmIdAndName);
                         break;
                     }
                 }
             }
-          initFarmNameSpinner(filteredFarmNames);
+            initFarmNameSpinner(filteredFarmNames);
         }
     }
+
+    private void initClicks() {
+        addFarmButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+
+                ActivityModel activityModel = new ActivityModel();
+                activityModel.setCultivation_id(cultivationPlan.getCultivation_ID());
+                activityModel.setActivity_name(activityNameSpinner.getSelectedItem().toString());
+                activityModel.setFarm_id(farmID);
+                activityModel.setPlanned_date(startDateTextView.getText().toString());
+                dismiss();
+
+            }
+        });
+
+        farmNameSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String input = farmNameSpinner.getSelectedItem().toString();
+                String[] parts = input.split(":");
+                farmID = parts[0].trim();
+                farmName = parts[1].trim();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+    }
+
 
     private void initSActivitySpinner() {
         String[] activity_type = {"Watering", "Fertilizing", "Pruning", "Mulching", "Harvesting", "Other"};
@@ -74,7 +116,6 @@ public class AddActivityCustomDialog extends Dialog {
         activityNameSpinner.setAdapter(activity_type_adapter);
     }
 
-
     private void initFarmNameSpinner(ArrayList<String> farms) {
         // Create an ArrayList to hold the filtered farm IDs and names
         // Create an adapter for the spinner using the filteredFarmNames list
@@ -82,6 +123,8 @@ public class AddActivityCustomDialog extends Dialog {
         farm_list_adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         // Set the adapter to the farmNameSpinner
         farmNameSpinner.setAdapter(farm_list_adapter);
+
     }
+
 
 }
